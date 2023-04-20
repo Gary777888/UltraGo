acf4d70444214264d0a9131cbe806a039bc8aa24
Gary777888 <wee_wee1995@hotmail.com>
2023-04-20 18:02:08 +0800

    Last min updated
    Updated last min

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ function App() {
    return (
       <div className="App">
          <NavBar />
+         <div>
+            <Footer />
+         </div>
       </div>
    );
 }
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,26 @@
 import React from "react";
-import "../../styles/Footer.scss";
+import "./Footer.css";
 import {
    faMapMarker,
    faPhone,
    faEnvelope,
    faGlobe,
    faClock,
+   faInstagram,
+   faUser,
+   instagram,
 } from "@fortawesome/free-solid-svg-icons";
+// import { faInstagram } from "@fortawesome/free-regular-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import AmericanexpressCardIcon from '../../assets/images/cards/americanexpress.png';
 // import CirrusCardIcon from '../../assets/images/cards/cirrus.png';
 // import MaterCardCardIcon from '../../assets/images/cards/mastercard.png';
 // import PaypalCardIcon from '../../assets/images/cards/paypal.png';
 // import VisaCardIcon from '../../assets/images/cards/visa.png';
+import InstagramIcon from "../instagram.png";
+import FacebookIcon from "../facebookicon.jpg";
+import TwitterIcon from "../twitterIcon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
    const date = new Date();
@@ -22,8 +30,8 @@ export default function Footer() {
          <div className="main-footer-container">
             <div className="main-footer-content">
                <div className="row">
-                  <div className="col-lg-4">
-                     <h2>Clayma store</h2>
+                  <div style={{ textAlign: "center" }}>
+                     <h2>UltraGo</h2>
                      <p>
                         We are providing high quality services with 100%
                         satisfaction guarantee.
@@ -33,44 +41,34 @@ export default function Footer() {
                            <li>
                               <a href="/">
                                  <img
-                                    src={AmericanexpressCardIcon}
-                                    alt="americaExpress"
+                                    src={InstagramIcon}
+                                    alt="InstaIcon"
+                                    style={{ width: "40px" }}
                                  />
                               </a>
                            </li>
                            <li>
                               <a href="/">
                                  <img
-                                    src={CirrusCardIcon}
-                                    alt="CirrusCardIcon"
+                                    src={FacebookIcon}
+                                    alt="FacebookIcon"
+                                    style={{ width: "40px" }}
                                  />
                               </a>
                            </li>
                            <li>
                               <a href="/">
                                  <img
-                                    src={MaterCardCardIcon}
-                                    alt="MaterCardCardIcon"
+                                    src={TwitterIcon}
+                                    alt="TwitterIcon"
+                                    style={{ width: "40px" }}
                                  />
-                              </a>
-                           </li>
-                           <li>
-                              <a href="/">
-                                 <img
-                                    src={PaypalCardIcon}
-                                    alt="PaypalCardIcon"
-                                 />
-                              </a>
-                           </li>
-                           <li>
-                              <a href="/">
-                                 <img src={VisaCardIcon} alt="VisaCardIcon" />
                               </a>
                            </li>
                         </ul>
                      </div>
                   </div>
-                  <div className="col-lg-4">
+                  {/* <div style={{ boxSizing: "borderBox" }}>
                      <h2>Contact us</h2>
                      <h5>
                         <FontAwesomeIcon
@@ -100,40 +98,15 @@ export default function Footer() {
                         />{" "}
                         www.claymastore.com
                      </h5>
-
-                     <h3> Customer Care</h3>
-
-                     <h5>
-                        <FontAwesomeIcon
-                           icon={faClock}
-                           className="footer-icons"
-                        />{" "}
-                        Mon. - Fri. 8am to 5pm
-                     </h5>
-                     <h5>
-                        <FontAwesomeIcon
-                           icon={faClock}
-                           className="footer-icons"
-                        />{" "}
-                        Sat. 8am to 11am
-                     </h5>
-                     <h5>
-                        <FontAwesomeIcon
-                           icon={faClock}
-                           className="footer-icons"
-                        />{" "}
-                        Sun.
-                        <span> Closed</span>
-                     </h5>
-                  </div>
-                  <div className="col-lg-4">
+                  </div> */}
+                  {/* <div className="col-lg-4">
                      <h2>My Account</h2>
                      <h5>Shopping Cart</h5>
                      <h5>My Account</h5>
                      <h5>Checkout</h5>
                      <h5>Delivery</h5>
                      <h5>Your Orders</h5>
-                  </div>
+                  </div> */}
                </div>
             </div>
             <div className="row">
